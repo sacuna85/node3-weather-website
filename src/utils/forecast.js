0c7b553fa8d43dafceb20683570bbd0abffdd9f4
unknown <acunass85@gmail.com>
2022-06-21 17:49:47 -0400

    Editing forcast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         }else if(body.error){
             callback('Unable to find location!', undefined)
         }else{
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out and feels like ${body.current.weather_description.feelslike}. There is a ${body.current.precip}% chance of rain.`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out and feels like ${body.current.weather_descriptions.feelslike}. There is a ${body.current.precip}% chance of rain.`)
         }
     })
 }
